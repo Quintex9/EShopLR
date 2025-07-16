c26eddc3cd2a99a6f423296a4f824e44e7360622
Quintex9 <misko.smatana@gmail.com>
2025-07-16 10:57:32 +0200

    Nainštalovanie a aktivovanie daisyui

--- resources/js/components/Core/TextInput.tsx
+++ resources/js/components/Core/TextInput.tsx
@@ -1,18 +1,7 @@
-import {
-    forwardRef,
-    InputHTMLAttributes,
-    useEffect,
-    useImperativeHandle,
-    useRef,
-} from 'react';
+import { forwardRef, InputHTMLAttributes, useEffect, useImperativeHandle, useRef } from 'react';
 
 export default forwardRef(function TextInput(
-    {
-        type = 'text',
-        className = '',
-        isFocused = false,
-        ...props
-    }: InputHTMLAttributes<HTMLInputElement> & { isFocused?: boolean },
+    { type = 'text', className = '', isFocused = false, ...props }: InputHTMLAttributes<HTMLInputElement> & { isFocused?: boolean },
     ref,
 ) {
     const localRef = useRef<HTMLInputElement>(null);
@@ -27,15 +16,5 @@ export default forwardRef(function TextInput(
         }
     }, [isFocused]);
 
-    return (
-        <input
-            {...props}
-            type={type}
-            className={
-                'rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600 ' +
-                className
-            }
-            ref={localRef}
-        />
-    );
+    return <input {...props} type={type} className={'input input-bordered' + className} ref={localRef} />;
 });
