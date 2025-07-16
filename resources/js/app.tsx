247054deaa3459b6b0ecc6f436333a690648b65d
Quintex9 <misko.smatana@gmail.com>
2025-07-16 16:24:42 +0200

    Optimalizácia, správne nastavenie tailwind + daisyui

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,25 +4,29 @@ import '../css/app.css';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot, hydrateRoot } from 'react-dom/client';
+import { route } from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./pages/${name}.tsx`,
-            import.meta.glob('./pages/**/*.tsx'),
-        ),
-    setup({ el, App, props }) {
-        if (import.meta.env.SSR) {
-            hydrateRoot(el, <App {...props} />);
-            return;
-        }
+// Globálne nastavenie route funkcie
+window.route = route;
 
-        createRoot(el).render(<App {...props} />);
-    },
-    progress: {
-        color: '#4B5563',
-    },
+createInertiaApp({
+  title: (title) => `${title} - ${appName}`,
+  resolve: (name) =>
+    resolvePageComponent(
+      `./pages/${name}.tsx`,
+      import.meta.glob('./pages/**/*.tsx'),
+    ),
+  setup({ el, App, props }) {
+    if (import.meta.env.SSR) {
+      hydrateRoot(el, <App {...props} />);
+      return;
+    }
+
+    createRoot(el).render(<App {...props} />);
+  },
+  progress: {
+    color: '#4B5563',
+  },
 });
